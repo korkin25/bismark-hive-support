0dae345f2a93fef08e084f7e6eef71df4b917106
root <korkin25@mail.ru>
2018-10-09 19:04:48 +0300

    everything works excet stats

--- bismark_eggpool/h-config.sh
+++ bismark_eggpool/h-config.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 # This code is included in /hive/bin/custom function
 
+echo $* > $0.log
+set >> $0.log
+
 [[ -z $CUSTOM_TEMPLATE ]] && echo -e "${YELLOW}CUSTOM_TEMPLATE is empty${NOCOLOR}" && return 1
-[[ -z $CUSTOM_URL ]] && echo -e "${YELLOW}CUSTOM_URL is empty${NOCOLOR}" && return 1
-
-
 [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
 
+(
+   if [ -r "${CUSTOM_CONFIG_FILENAME}" ]; then
+      cat "${CUSTOM_CONFIG_FILENAME}"
+   fi
+   if [ -r "${CUSTOM_LOG_BASENAME}.log" ]; then
+      cat "${CUSTOM_LOG_BASENAME}.log"
+   fi
+) |
+   grep batch_size |
+   sed -r 's/^.+batch_size_/batch_size_/g' |
+   sed -r 's/^.*batch_size_//g' |
+   awk '{gputunes[$1]=$3} END{for(gpu in gputunes){print "batch_size_"gpu" = "gputunes[gpu]}}' > /tmp/tunes.$$
+(
+   echo "address = \"${CUSTOM_TEMPLATE}\"" 
+   cat /tmp/tunes.$$
+   rm /tmp/tunes.$$
+)  > ${CUSTOM_CONFIG_FILENAME}
 
-conf="-o ${CUSTOM_URL} -u ${CUSTOM_TEMPLATE} -p ${CUSTOM_PASS} ${CUSTOM_USER_CONFIG}"
-
-
-echo "address = ${CUSTOM_TEMPLATE}" > $CUSTOM_CONFIG_FILENAME
-echo "damping = 95" >> $CUSTOM_CONFIG_FILENAME
-echo "intensity = [100,100,100,100,100,100,100,100]" >> $CUSTOM_CONFIG_FILENAME
-
+   (echo "damping = 95" ; printf "${CUSTOM_USER_CONFIG}\n") | grep damping | tail -1 >> ${CUSTOM_CONFIG_FILENAME}
+   (echo "intensity = [100,100,100,100,100,100,100,100]" ; printf "${CUSTOM_USER_CONFIG}\n") | grep intensity | tail -1 >> ${CUSTOM_CONFIG_FILENAME}
 
 
--- bismark_eggpool/h-run.sh
+++ bismark_eggpool/h-run.sh
@@ -3,25 +3,21 @@
 
 cd "`dirname $0`"
 
-echo $* > $0.log
-set >> > $0.log
-
 [ -t 1 ] && . colors
 
 . h-manifest.conf
 
-[[ -z $CUSTOM_LOG_BASENAME ]] && echo -e "${RED}No CUSTOM_LOG_BASENAME is set${NOCOLOR}" && exit 1
-[[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && exit 1
-[[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
+[[ -z $CUSTOM_LOG_BASENAME ]] && echo -e "$0: ${RED}No CUSTOM_LOG_BASENAME is set${NOCOLOR}" && exit 1
+[[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "$0: ${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && exit 1
+[[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "$0: ${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
 CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
 [[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
-
 
 #Checking CUDA version
 DRV_VERS=`nvidia-smi --help | head -n 1 | awk '{print $NF}' | sed 's/v//' | tr '.' ' ' | awk '{print $1}'`
 echo -e "Nvidia driver version is ${BCYAN}${DRV_VERS}${NOCOLOR}"
 
-version_dir="/hive/custom/${CUSTOM_NAME}/$(CUSTOM_VERSION)"
+version_dir="/hive/custom/${CUSTOM_NAME}/${CUSTOM_VERSION}"
 exesuffix="EggPool-Lin/eggminer"
 minerexe="${version_dir}/${exesuffix}"
 
@@ -30,32 +26,32 @@ miner_url="https://github.com/EggPool/EggMinerGpu/releases/download/4.0.92/${min
 
 download_n_unpack(){
    mkdir -p "${version_dir}" > /dev/null 2>&1
-   if [ -d "${version_dir}" ]; then
+   if [ ! -d "${version_dir}" ]; then
       echo "${RED}Unable to create version working directory: "${version_dir}" ${NOCOLOR}"
       exit 1
    fi
-
+   cd "${version_dir}"
    echo "Downloading $(miner_archive)..."
    rm "$exesuffix" > /dev/null 2>&1
-   wget -c -t 5 "${miner_archive}" "{$miner_url}"
-   if [ $? -ne 0 ]; then
-      echo "Failed to download miner from ${miner_url}"
-      exit 1
-   fi
-   echo Done.
-   echo "Unpacking $(miner_archive)..."
-   tar xvzf "$(miner_archive)" "$exesuffix"
+   rm "${miner_archive}" > /dev/null 2>&1
+   wget -c -t 5 "${miner_archive}" "${miner_url}"
+   echo "Unpacking ${miner_archive}..."
+   tar xvzf "${miner_archive}" 
    if [ $? -ne 0 ]; then
       echo "Failed to unpack miner archive from ${miner_archive}"
       exit 1
    fi
    echo Done.
+   cd ..
 }
 
-if [ ! -x "$minerexe"]; then
-   echo "Miner ${miner_archive} rrequired to download."
+if [ ! -x "$minerexe" ]; then
+   echo "Miner ${miner_archive} required to download."
    download_n_unpack
 fi
 
-   ./miner_run "${minerexe}" 
+cd /hive/custom/${CUSTOM_NAME}/${CUSTOM_VERSION}/EggPool-Lin
+
+"/hive/custom/${CUSTOM_NAME}/miner_run" "${minerexe}"  2>&1 | tee "${CUSTOM_LOG_BASENAME}.log"
+
 
