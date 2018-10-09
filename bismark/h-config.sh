d16073bdf4ab1811c8458304d5baf302ed4f1106
root <korkin25@mail.ru>
2018-10-09 14:18:52 +0300

    h-manifest.conf  done

--- bismark/h-config.sh
+++ bismark/h-config.sh
@@ -4,11 +4,7 @@
 [[ -z $CUSTOM_TEMPLATE ]] && echo -e "${YELLOW}CUSTOM_TEMPLATE is empty${NOCOLOR}" && return 1
 [[ -z $CUSTOM_URL ]] && echo -e "${YELLOW}CUSTOM_URL is empty${NOCOLOR}" && return 1
 
-algo=${CUSTOM_ALGO}
-
-[[ ! -z ${algo} ]] && algo="-a ${algo}"
-
-conf="${algo} -o ${CUSTOM_URL} -u ${CUSTOM_TEMPLATE} -p ${CUSTOM_PASS} ${CUSTOM_USER_CONFIG}"
+conf="-o ${CUSTOM_URL} -u ${CUSTOM_TEMPLATE} -p ${CUSTOM_PASS} ${CUSTOM_USER_CONFIG}"
 
 [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
 echo "$conf" > $CUSTOM_CONFIG_FILENAME
--- bismark/h-run.sh
+++ bismark/h-run.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
+#testgit
+
+echo $* > $0.log
+set >> > $0.log
+
 
 cd "`dirname $0`"
 
-. colors
+[ -t 1 ] && . colors
 
 . h-manifest.conf
 
+[[ -z $CUSTOM_LOG_BASENAME ]] && echo -e "${RED}No CUSTOM_LOG_BASENAME is set${NOCOLOR}" && exit 1
 [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && exit 1
 [[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
 CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
 [[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
 
 
-version_dir="/hive/custom/${CUSTOM_NAME}/${CUSTOM_VERSION}"
+version_dir="/hive/custom/${CUSTOM_NAME}/$(CUSTOM_VERSION)"
 
 #Checking CUDA version
 DRV_VERS=`nvidia-smi --help | head -n 1 | awk '{print $NF}' | sed 's/v//' | tr '.' ' ' | awk '{print $1}'`
-echo "Driver version is ${BCYAN}${DRV_VERS}${NOCOLOR}"
+echo -e "Driver version is ${BCYAN}${$DRV_VERS}${NOCOLOR}"
 
 if [ ${DRV_VERS} -ge 396 ]; then
    cudaver="9.2"
@@ -31,38 +37,37 @@ else
 fi
 
 miner_archive="zjazz_cuda${cudaver}_linux_suqa_0.991.tar.gz"
-miner_url="https://github.com/zjazz/zjazz_cuda_miner_experimental/releases/download/x22i_0991/${miner_archive}"
+miner_url="https://github.com/zjazz/zjazz_cuda_miner_experimental/releases/download/x22i_0991/$(miner_archive)"
 
 download_n_unpack(){
    mkdir -p "${version_dir}" > /dev/null 2>&1
-   if [ ! -d "${version_dir}" ]; then
-      echo "Unable to create version working directory: ${version_dir}"
+   if [ -d "${version_dir}" ]; then
+      echo "${RED}Unable to create version working directory: "${version_dir}" ${NOCOLOR}"
       exit 1
    fi
 
    cd "${version_dir}"
-   echo "Downloading ${miner_archive}..."
+   echo "Downloading $(miner_archive)..."
    rm "$exesuffix" > /dev/null 2>&1
-   wget -c -t 1 "${miner_url}"
+   curl -o "${miner_archive}" "{$miner_url}"
    if [ $? -ne 0 ]; then
       echo "Failed to download miner from ${miner_url}"
       exit 1
    fi
    echo Done.
-   echo "Unpacking ${miner_archive}..."
-   tar xvzf "${miner_archive}" "${exesuffix}"
+   echo "Unpacking $(miner_archive)..."
+   tar xvzf "$(miner_archive)" "$exesuffix"
    if [ $? -ne 0 ]; then
       echo "Failed to unpack miner archive from ${miner_archive}"
       exit 1
    fi
    echo Done.
-   cd ..
 }
 
-if [ ! -x "${minerexe}" ]; then
-   echo "Miner ${miner_archive} required to download."
+if [ ! -x "$minerexe"]; then
+   echo "Miner ${miner_archive} rrequired to download."
    download_n_unpack
 fi
 
-   ./zjazz_cuda_run "${minerexe}" $(< $CUSTOM_CONFIG_FILENAME) -L "${CUSTOM_CONFIG_FILENAME}" $@
+   ./zjazz_cuda_run "${minerexe}" $(< $CUSTOM_CONFIG_FILENAME) --log $CUSTOM_LOG_BASENAME.log -b ${WEB_HOST}:${WEB_PORT}$@
 
