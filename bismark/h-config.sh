45ed04d1ef53696791a5a8dac0c07ff4c0731e5c
root <korkin25@mail.ru>
2018-10-09 14:28:59 +0300

    h-config.sh done

--- bismark/h-config.sh
+++ bismark/h-config.sh
@@ -4,8 +4,16 @@
 [[ -z $CUSTOM_TEMPLATE ]] && echo -e "${YELLOW}CUSTOM_TEMPLATE is empty${NOCOLOR}" && return 1
 [[ -z $CUSTOM_URL ]] && echo -e "${YELLOW}CUSTOM_URL is empty${NOCOLOR}" && return 1
 
-conf="-o ${CUSTOM_URL} -u ${CUSTOM_TEMPLATE} -p ${CUSTOM_PASS} ${CUSTOM_USER_CONFIG}"
 
 [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && return 1
-echo "$conf" > $CUSTOM_CONFIG_FILENAME
+
+
+conf="-o ${CUSTOM_URL} -u ${CUSTOM_TEMPLATE} -p ${CUSTOM_PASS} ${CUSTOM_USER_CONFIG}"
+
+
+echo "address = ${CUSTOM_TEMPLATE}" > $CUSTOM_CONFIG_FILENAME
+echo "damping = 95" >> $CUSTOM_CONFIG_FILENAME
+echo "intensity = [100,100,100,100,100,100,100,100]" >> $CUSTOM_CONFIG_FILENAME
+
+
 
