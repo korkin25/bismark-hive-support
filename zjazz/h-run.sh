89e5642a57f0463e77c421580dcb4be7a8cc0dbc
root <korkin25@mail.ru>
2018-10-09 09:05:50 +0300

    works everything except stats

--- zjazz/h-run.sh
+++ zjazz/h-run.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
-#test atom
-
-echo $* > $0.log
-set >> > $0.log
-
 
 cd "`dirname $0`"
 
-[ -t 1 ] && . colors
+. colors
 
 . h-manifest.conf
 
-[[ -z $CUSTOM_LOG_BASENAME ]] && echo -e "${RED}No CUSTOM_LOG_BASENAME is set${NOCOLOR}" && exit 1
 [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && exit 1
 [[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
 CUSTOM_LOG_BASEDIR=`dirname "$CUSTOM_LOG_BASENAME"`
 [[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
 
 
-version_dir="/hive/custom/${CUSTOM_NAME}/$(CUSTOM_VERSION)"
+version_dir="/hive/custom/${CUSTOM_NAME}/${CUSTOM_VERSION}"
 
 #Checking CUDA version
 DRV_VERS=`nvidia-smi --help | head -n 1 | awk '{print $NF}' | sed 's/v//' | tr '.' ' ' | awk '{print $1}'`
-echo -e "Driver version is ${BCYAN}${$DRV_VERS}${NOCOLOR}"
+echo "Driver version is ${BCYAN}${DRV_VERS}${NOCOLOR}"
 
 if [ ${DRV_VERS} -ge 396 ]; then
    cudaver="9.2"
@@ -37,36 +31,38 @@ else
 fi
 
 miner_archive="zjazz_cuda${cudaver}_linux_suqa_0.991.tar.gz"
-miner_url="https://github.com/zjazz/zjazz_cuda_miner_experimental/releases/download/x22i_0991/$(miner_archive)"
+miner_url="https://github.com/zjazz/zjazz_cuda_miner_experimental/releases/download/x22i_0991/${miner_archive}"
 
 download_n_unpack(){
    mkdir -p "${version_dir}" > /dev/null 2>&1
-   if [ -d "${version_dir}" ]; then
-      echo "${RED}Unable to create version working directory: "${version_dir}" ${NOCOLOR}"
+   if [ ! -d "${version_dir}" ]; then
+      echo "Unable to create version working directory: ${version_dir}"
       exit 1
    fi
 
    cd "${version_dir}"
-   echo "Downloading $(miner_archive)..."
+   echo "Downloading ${miner_archive}..."
    rm "$exesuffix" > /dev/null 2>&1
-   curl -o "${miner_archive}" "{$miner_url}"
+   wget -c -t 1 "${miner_url}"
    if [ $? -ne 0 ]; then
       echo "Failed to download miner from ${miner_url}"
       exit 1
    fi
    echo Done.
-   echo "Unpacking $(miner_archive)..."
-   tar xvzf "$(miner_archive)" "$exesuffix"
+   echo "Unpacking ${miner_archive}..."
+   tar xvzf "${miner_archive}" "${exesuffix}"
    if [ $? -ne 0 ]; then
       echo "Failed to unpack miner archive from ${miner_archive}"
       exit 1
    fi
    echo Done.
+   cd ..
 }
 
-if [ ! -x "$minerexe"]; then
-   echo "Miner ${miner_archive} rrequired to download."
+if [ ! -x "${minerexe}" ]; then
+   echo "Miner ${miner_archive} required to download."
    download_n_unpack
 fi
 
-   ./zjazz_cuda_run "${minerexe}" $(< $CUSTOM_CONFIG_FILENAME) --log $CUSTOM_LOG_BASENAME.log -b ${WEB_HOST}:${WEB_PORT}$@
+   ./zjazz_cuda_run "${minerexe}" $(< $CUSTOM_CONFIG_FILENAME) -L "${CUSTOM_CONFIG_FILENAME}" $@
+
--- zjazz/h-stats.sh
+++ zjazz/h-stats.sh
@@ -1,141 +1,67 @@
 #!/usr/bin/env bash
-echo $* > $0.log
-set >> > $0.log
-
-cd `dirname $0`
-#. h-manifest.conf
-#. debug.conf
-#. /hive-config/wallet.conf
-. /hive/custom/$CUSTOM_MINER/h-manifest.conf
-
-#API_PORT=4058
-#API_TIMEOUT=5
-algo_avail=("balloon" "bcd" "bitcore" "c11" "hmq1725" "hsr" "lyra2z" "phi" "polytimos" "renesis" "skunk" "sonoa" "tribus" "x16r" "x16s" "x17")
-
-#######################
-# Functions
-#######################
-
-
-get_cards_hashes(){
-	# hs is global
-	for (( i=0; i < ${GPU_COUNT_NVIDIA}; i++ )); do
-		hs[$i]=0
-	done
-	local gpu_total=$(echo $miner_stat | jq ".gpu_total")
-	for (( i=0; i < ${gpu_total}; i++ )); do
-		local gpu_id=$(echo $miner_stat | jq ".gpus[$i].gpu_id")
-		local hashrate=$(echo $miner_stat | jq ".gpus[$i].hashrate")
-		hs[$gpu_id]=$((hashrate/1000))
-	done
-#	for (( i=0; i < ${GPU_COUNT_NVIDIA}; i++ )); do
-#		hs[$i]=0
-#	done
-}
-
-get_nvidia_cards_temp(){
-	echo $(jq -c "[.gpus[].temperature]" <<< $miner_stat)
-}
-
-get_nvidia_cards_fan(){
-	echo $(jq -c "[.gpus[].fan_speed]" <<< $miner_stat)
-}
-
-get_miner_uptime(){
-	local uptime=$(echo $miner_stat | jq ".uptime")
-	[[ $uptime == "null" ]] && uptime=0
-	echo $uptime
-}
-
-get_miner_algo(){
-#	local algo="x16r"
-#	
-#	for i in "${algo_avail[@]}"
-#	do
-#		if [[ ! -z $(echo $CUSTOM_USER_CONFIG | grep $i) ]]; then
-#			algo=$i
-#			break
-#		fi
-#	done
-#	echo $algo
-	echo $(jq -r -c '.algorithm' <<< "$miner_stat")
-}
-
-get_miner_shares_ac(){
-	local acc=$(echo $miner_stat | jq ".accepted_count")
-	[[ $acc == "null" ]] && acc=0
-	echo $acc
-}
-
-get_miner_shares_rj(){
-	local rej=$(echo $miner_stat | jq ".rejected_count")
-	[[ $rej == "null" ]] && rej=0
-	echo $rej
-}
-
-get_total_hashes(){
-	local total=$(echo $miner_stat | jq ".hashrate")
-	[[ $total == "null" ]] && total=0
-	echo $((total/1000))
-}
 
 #######################
 # MAIN script body
 #######################
 
+	threads=`echo "threads" | nc -w $API_TIMEOUT localhost ${API_PORT}` #&& echo $threads
+	if [[ $? -ne 0  || -z $threads ]]; then
+		echo -e "${YELLOW}Failed to read $miner stats from localhost:${API_PORT}${NOCOLOR}"
+	else
+		summary=`echo "summary" | nc -w 2 localhost ${API_PORT}`
 
-miner_stat=`echo 'summary' | nc -w $API_TIMEOUT localhost $API_PORT`
+		re=';UPTIME=([0-9]+);' && [[ $summary =~ $re ]] && local uptime=${BASH_REMATCH[1]} #&& echo "Matched" || echo "No match"
+		#khs will calculate from cards; re=';KHS=([0-9\.]+);' && [[ $summary =~ $re ]] && khs=${BASH_REMATCH[1]} #&& echo "Matched" || echo "No match"
+		algo=`echo "$summary" | tr ';' '\n' | grep -m1 'ALGO=' | sed -e 's/.*=//'`
 
-#echo $miner_stat
+		local ac=`echo "$summary" | tr ';' '\n' | grep -m1 'ACC=' | sed -e 's/.*=//'`
+		local rj=`echo "$summary" | tr ';' '\n' | grep -m1 'REJ=' | sed -e 's/.*=//'`
+		#stats=`echo $threads | tr '|' '\n' | tr ';' '\n' | tr -cd '\11\12\15\40-\176' | grep -E 'KHS=' | sed -e 's/.*=//' | jq -cs '{khs:.}'`
+		striplines=`echo "$threads" | tr '|' '\n' | tr ';' '\n' | tr -cd '\11\12\15\40-\176'`
 
-if [[ $? -ne 0 ]]; then
-	echo -e "${YELLOW}Failed to read miner stats from localhost:${API_PORT}${NOCOLOR}"
-	stats=""
-	khs=0
-	return 1
-fi
+		#if GPU has 0.0 temp it hanged. ccminer does not mine on this card but shows hashrate
+		cctemps=(`echo "$striplines" | grep 'TEMP=' | sed -e 's/.*=//'`) #echo ${cctemps[@]} | tr " " "\n" #print it in lines
+		cckhs=(`echo "$striplines" | grep 'KHS=' | sed -e 's/.*=//'`)
+		ccbusids=(`echo "$striplines" | grep 'BUS=' | sed -e 's/.*=//'`)
 
-#miner_stat=`cat stat.json`
 
-[[ -z $GPU_COUNT_NVIDIA ]] &&
-	GPU_COUNT_NVIDIA=`gpu-detect NVIDIA`
+		#local nvidiastats
+		for (( i=0; i < ${#cckhs[@]}; i++ )); do
+			#if temp is 0 then driver or GPU failed
+			[[ ${cctemps[$i]} == "0.0" ]] && cckhs[$i]="0.0"
 
-	get_cards_hashes					# hashes array
-	hs_units='khs'						# hashes utits
-	local temp=$(get_nvidia_cards_temp)		# cards temp
-	local fan=$(get_nvidia_cards_fan)			# cards fan
-	uptime=$(get_miner_uptime)			# miner uptime
-	algo=$(get_miner_algo)				# algo
-	# A/R shares by pool
-	ac=$(get_miner_shares_ac)
-	rj=$(get_miner_shares_rj)
-	
+			#cckhs[$i]="84316579.94" #test
+			#check Ghs. 1080ti gives ~64mh (64000kh) on lyra. when it's showing ghs then load is 0 on gpu
+			#if [[ `echo ${cckhs[$i]} | awk '{ print ($1 >= 1000000) ? 1 : 0 }'` == 1 ]]; then #hash is in Ghs, >= 1000000 khs
+			if [[ `echo ${cckhs[$i]} | awk '{ print ($1 >= 1000) ? 1 : 0 }'` == 1 ]]; then # > 1Mh
+			#[[ -z $nvidiastats ]] && nvidiastats=`gpu-stats nvidia` #a bit overhead in calling nvidia-smi again
+				local busid=`echo ${ccbusids[$i]} | awk '{ printf("%02x:00.0", $1) }'` #ccbus is decimal
+				local load_i=`echo "$gpu_stats" | jq ".busids|index(\"$busid\")"`
+				if [[ $load_i != "null" ]]; then #can be null on failed driver
+					local load=`echo "$gpu_stats" | jq -r ".load[$load_i]"`
+					#load=0 #test
+					[[ -z $load || $load -le 10 ]] &&
+						echo -e "${RED}Hash on GPU$i is in GH/s (${cckhs[$i]} kH/s) but Load is detected to be only $load%${NOCOLOR}" &&
+						cckhs[$i]="0.0"
+				fi
+			fi
 
-#	# make JSON
-	stats=$(jq -nc \
-				--argjson hs "`echo ${hs[@]} | tr " " "\n" | jq -cs '.'`" \
-				--arg hs_units "$hs_units" \
-				--argjson temp "$temp" \
-				--argjson fan "$fan" \
-				--arg uptime "$uptime" \
-				--arg ac $ac --arg rj "$rj" \
-				--arg algo "$algo" \
-				'{$hs, $hs_units, $temp, $fan, $uptime, ar: [$ac, $rj], $algo}')
+     #khs=`echo $khs ${cckhs[$i]} | awk '{ printf("%.3f", $1 + $2) }'`
+			khs=`echo $khs ${cckhs[$i]} | awk '{ printf("%.3f", $1 + $2) }'`
+		done
 
-	# total hashrate in khs
-	khs=$(get_total_hashes)
+		khs=`echo $khs | sed -E 's/^( *[0-9]+\.[0-9]([0-9]*[1-9])?)0+$/\1/'` #1234.100 -> 1234.1
+		hs=''
 
-# debug output
-#echo temp:   $temp
-#echo fan:    $fan
-#echo stats:  $stats
-#echo khs:    $khs
-#echo uptime: $uptime
+		stats=$(jq -n \
+				--arg uptime "$uptime", --arg algo "$algo" \
+				--argjson hs "`echo ${cckhs[@]} | tr " " "\n" | jq -cs '.'`" \
+				--argjson temp "`echo ${cctemps[@]} | tr " " "\n" | jq -cs '.'`" \
+				--argjson fan "`echo \"$striplines\" | grep 'FAN=' | sed -e 's/.*=//' | jq -cs '.'`" \
+				--arg ac "$ac" --arg rj "$rj" \
+				'{$hs, $temp, $fan, $uptime, $algo, ar: [$ac, $rj]}')
+	fi
 
-##echo hs:       ${hs[@]}
-##echo hs_units: $hs_units
-##echo temp:     $temp
-##echo fan:      $fan
-##echo uptime    $uptime
-##echo "ac/rj":  $ac $rj
-##echo algo:     $algo
+	[[ -z $khs ]] && khs=0
+	[[ -z $stats ]] && stats="null"
+
